7b9ff87389b3b1a5548c8dbf26bf932771452cac
Jerry-chou-zhi <157726093+Jerry-chou-zhi@users.noreply.github.com>
2024-11-19 23:46:23 +0800

    Update OrientationProcessor.py

--- backend/OrientationProcessor.py
+++ backend/OrientationProcessor.py
@@ -4,14 +4,19 @@ import numpy as np
 
 class OrientationProcessor:
     __slots__ = [
-        "datacube",  # 存储加载的数据集
+        "datacube",  
+        "datacube_probe",
+        "_dp_mean",
+        "_probe_dp_mean",
+        "_dp_max",
+        "_geometry_adf",
         "_orientation_plan",  # 取向计划对象
         "_orientation_map",  # 计算生成的取向映射
         "_strain_map",  # 应变映射对象
         "_grain_boundaries",  # 晶界数据
-        "_rxs",  # 选择区域的x坐标列表
-        "_rys",  # 选择区域的y坐标列表
-        "_colors",  # 颜色映射，用于可视化
+        "_rxs", 
+        "_rys", 
+        "_colors", 
         "_orientation_params",  # 用于创建取向计划的参数
         "_strain_params",  # 用于计算应变的参数
         "_grain_boundary_params",  # 用于提取晶界的参数
@@ -22,24 +27,139 @@ class OrientationProcessor:
         "_amorph_ROI",  # 无序环区域
         "_p_ellipse",  # 椭圆拟合结果参数
         "_p_dsg",  # 椭圆拟合图形化参数
-        "_adf",  # 暗场图像
-        "_bf",  # 亮场图像
-        "_probe",  # 探针数据
+        "_adf", 
+        "_bf", 
+        "_probe_ROI",
+        "_probe", 
+        "_probe_semiangle",
+        "_probe_qx0",
+        "_probe_qy0",
         "_kernel",  # 探针核
         "_probe_params",  # 探针的初始化参数
         "_disk_detect_params",  # Bragg盘检测的参数
     ]
-    def __init__(self):
-        self.datacube = None
+    def set_probe_xlims_ylims(self, xlims: tuple, ylims: tuple):
+        self._probe_xlims = xlims
+        self._probe_ylims = ylims
+        self._probe_ROI = np.zeros(self.datacube.rshape, dtype=bool)
+        self._probe_ROI[
+            self._probe_xlims[0] : self._probe_xlims[1],
+            self._probe_ylims[0] : self._probe_ylims[1],
+        ] = True
+   
+    def get_kernel_image(self):
+        fig = py4DSTEM.visualize.show_kernel(
+            self._probe.kernel,
+            R=20,
+            L=20,
+            W=1,
+            figsize = (8,4),
+            returnfig=True,
+        )
+        return fig[0]
+        
+    def set_probe(self):
+        self._probe = self.datacube.get_vacuum_probe(ROI=self._probe_ROI) #没改
+        self._probe_semiangle, self._probe_qx0, self._probe_qy0 = (
+            py4DSTEM.process.calibration.get_probe_size(self._probe.probe)#没改
+        )
+        self._probe.get_kernel(
+            mode="sigmoid",
+            #原来的origin删了
+            radii=(self._probe_semiangle * 0, self._probe_semiangle * 4), #改动
+            bilinear = True, #改动
+        )
+  
+    def get_points_image(self):
+        fig = py4DSTEM.visualize.show_points(
+            self._adf,
+            x=self._rxs,
+            y=self._rys,
+            pointcolor=self._colors,
+            returnfig=True,
+        )
+        return fig[0]
+    
+    def set_rxs_rys(self, rxs: list, rys: list):
+        self._rxs = rxs
+        self._rys = rys
+         
+    def get_adf_image(self):
+        fig = show([self._adf], bordercolor="w", cmap="gray", returnfig=True)
+        return fig[0]
+    
+    def set_geometry_adf(self, center: tuple, radii: tuple):
+        self._geometry_adf = (center, radii)
+        
+    def get_position_detector_image(self):
+        fig = self.datacube.position_detector(
+            data=self._dp_max,
+            mode="annular",
+            geometry=self._geometry_adf,
+            returnfig=True,
+        )
+        return fig[0]
+        
+    def calc_adf(self):
+        self._adf = self.datacube.get_virtual_image(
+            mode="annulus",
+            geometry=self._geometry_adf,
+            name="dark_field",
+        )
+        
+    def calibrate_diffraction_space(self, pixel_size_inv_ang, units="A^-1"):
+        self.datacube.calibration.set_Q_pixel_size(pixel_size_inv_ang)
+        self.datacube.calibration.set_Q_pixel_units(units)
+        self.datacube_probe.calibration.set_Q_pixel_size(pixel_size_inv_ang)
+        self.datacube_probe.calibration.set_Q_pixel_units(units)
+
+    def calibrate_real_space(self, pixel_size_ang, units="A"):
+        self.datacube.calibration.set_R_pixel_size(pixel_size_ang)
+        self.datacube.calibration.set_R_pixel_units(units)
+
+    def get_dp_image(self, vmax=1, power=0.5, cmap="inferno"):
+        fig = py4DSTEM.show(
+            [
+                self._dp_mean,
+                self._dp_max,
+                self._probe_dp_mean,
+            ],
+            vmax=vmax,
+            power=power,
+            cmap=cmap,
+            returnfig=True,
+        )
+        return fig[0]  
+        
+    def load_data(self, data_file_path: str, probe_file_path: str):      
+        self.datacube = py4DSTEM.import_file(filepath=data_file_path)
+        self._dp_mean = self.datacube.get_dp_mean()
+        self._dp_max = self.datacube.get_dp_max()
+        self.datacube_probe = py4DSTEM.import_file(filepath=probe_file_path)
+        self._probe_dp_mean = self.datacube_probe.get_dp_mean()
+        self._geometry_adf = None
+        self._bf = None
+        self._adf = None
+        self._rxs = None
+        self._rys = None
         self.orientation_plan = None
         self.orientation_map = None
         self.strain_map = None
         self.grain_boundaries = None
-
-    def load_dataset(self, file_path):
-        """Load the dataset from a file."""
-        self.datacube = py4DSTEM.import_file(file_path)
-
+        self._colors = [
+            "deeppink",
+            "coral",
+            "gold",
+            "chartreuse",
+            "dodgerblue",
+            "rebeccapurple",
+        ]
+        self._probe = None
+        self._probe_ROI = None
+        self._probe_semiangle = 4 #改动
+        self._probe_qx0 = None
+        self._probe_qy0 = None
+        
     def create_orientation_plan(self, params):
         """
         Create an orientation plan for crystal alignment.
